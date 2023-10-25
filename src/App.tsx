de1ec774415d4d85fcb0f38f651e26cab1dfc74f
Sebastian Sandoval <sebassandoval@hotmail.com>
2023-10-25 08:45:11 -0500

    Add initial configuration and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="w-full flex flex-col items-center justify-center">
+      <h2>Hello world!</h2>
+    </main>
+  )
+}
+
+export default App
